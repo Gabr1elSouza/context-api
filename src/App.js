6641847f70ba7c59c6e7e8c94dee36854f330117
Gabriel Souza <126912751+Gabr1elSouza@users.noreply.github.com>
2024-01-18 15:22:36 -0300

    `Create Context`

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//components
+import Navbar from "./components/Navbar";
+
+//pages
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Products from "./Pages/Products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <h1>Context</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
